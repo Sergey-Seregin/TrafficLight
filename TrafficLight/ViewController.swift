05d0476dfba7754139436f824400a3451086801d
Sergey Seregin <sergey.seregin.kz@gmail.com>
2022-03-02 23:40:08 +0600

    Lights now is circle, add logic switch lights

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var nextLightButton: UIButton!
+    
+    var nextLight = TrafficLight.red
+    
+    enum TrafficLight {
+        case red
+        case yellow
+        case green
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        redLight.layer.cornerRadius = 100
+        yellowLight.layer.cornerRadius = 100
+        greenLight.layer.cornerRadius = 100
+        
+        turnOffLights()
     }
 
-
+    @IBAction func nextLightButtonPressed(_ sender: UIButton) {
+        nextLightButton.setTitle("NEXT", for: .normal)
+        turnOffLights()
+             
+        switch nextLight {
+        case .red:
+            nextLight = .yellow
+            redLight.backgroundColor = UIColor.red.withAlphaComponent(1)
+        case .yellow:
+            nextLight = .green
+            yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(1)
+        case .green:
+            nextLight = .red
+            greenLight.backgroundColor = UIColor.green.withAlphaComponent(1)
+        }
+    }
+    
+    private func turnOffLights() {
+        redLight.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+        yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+        greenLight.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+    }
 }
-
